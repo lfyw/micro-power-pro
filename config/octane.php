3a57378a08b1b321bb01614bd858389204f77c9d
littledragoner <1559897062@qq.com>
2022-01-11 16:24:35 +0800

    chore: ide-helper

--- config/octane.php
+++ config/octane.php
@@ -217,4 +217,11 @@ return [
 
     'max_execution_time' => 30,
 
+    'swoole' => [
+        'options' => [
+            'log_file' => storage_path('logs/swoole_http.log'),
+            'package_max_length' => 10 * 1024 * 1024,
+        ],
+    ]
+
 ];
